85ca6dabe7100ecf73a48fa4103211fa43e7d91c
Raphael Dylan Dalida <raphaeldylandalida@gmail.com>
2024-11-19 12:42:51 -0800

    fixed fine-tuner

--- finetune.py
+++ finetune.py
@@ -3,21 +3,24 @@ import requests
 
 url = "https://api.openpipe.ai/api/v1/unstable/finetune/create"
 
+dataset_id_tiny = "e231526d-09da-4cf8-9713-08bd68eebea7"
+dataset_id_all = "476d7c11-48ef-4e59-be41-5892fb47e508"
+
 payload = {
-    "datasetId": "476d7c11-48ef-4e59-be41-5892fb47e508",
-    "slug": "finetune-race",
+    "datasetId": dataset_id_all,
+    "slug": "finetune-race-full-4",
     "baseModel": "meta-llama/Meta-Llama-3.1-8B-Instruct",
     "overrides": {
         "batch_size": "auto",
-        "learning_rate_multiplier": 123,
-        "num_epochs": 123,
-        "preference_tuning_variant": "DPO",
-        "preference_tuning_learning_rate_multiplier": 123,
-        "preference_tuning_num_epochs": 123,
-        "preference_tuning_training_beta": 123,
-        "preference_tuning_adapter_weight": 123,
-        "is_sft_enabled": True,
-        "is_preference_tuning_enabled": True
+        "learning_rate_multiplier": 1.2,
+        "num_epochs": 2,
+        # "preference_tuning_variant": "DPO",
+        # "preference_tuning_learning_rate_multiplier": 2,
+        # "preference_tuning_num_epochs": 2,
+        # "preference_tuning_training_beta": 2,
+        # "preference_tuning_adapter_weight": 2,
+        # "is_sft_enabled": True,
+        # "is_preference_tuning_enabled": True
     }
 }
 headers = {
